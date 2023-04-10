af7800abce642c763b9a59f0853837c218bc2650
Michel Cadennes <michel.cadennes@assemblee-virtuelle.org>
2023-04-10 12:49:55 +0200

    Ajout de classes de validation de requêtes
    Ces classes traitent deux catégories de formulaires :
    - Création de profil utilisateur (StoreRegisterRequest)
    - Création d'un type de produit (StoreProductTypeRequest)

---
+++ app/Http/Requests/StoreRegisterRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return (! auth()->user());
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:8',
+            'address' => 'required',
+            'city' => 'required',
+            'postal_code' => 'required|regex:/[0-9][1-9][0-9]{3}/',
+            'phone' => 'regex:/0[0-9]{9}/',
+        ];
+    }
+}
